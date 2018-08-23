25239ed7bc8208d6f376d44faf7c29fc215156be
benjaminsinaiko <ben.jamin@outlook.com>
2018-08-23 00:24:13 -0500

    More cleanup

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <nav className="navbar navbar-expand-sm navbar-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
             FPL 2018/19
