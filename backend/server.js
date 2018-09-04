912651d4d3a372dcc5134ce3e7c4143fea5d749b
benjaminsinaiko <ben.jamin@outlook.com>
2018-09-04 13:12:46 -0500

    Prepared for heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const players = require('./routes/api/players');
 const teams = require('./routes/api/teams');
@@ -40,6 +41,16 @@ app.use('/api/players', players);
 app.use('/api/teams', teams);
 app.use('/api/leagues', leagues);
 app.use('/api/results', results);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.get('/', (req, res) => res.send(process.env.MONGO_USER));
 
