b8582156d377976a95e7302862a56047ac7e63e4
benjaminsinaiko <ben.jamin@outlook.com>
2018-10-13 18:25:45 -0500

    Set gameweeks data from player info in results container

--- new-client/src/containers/LeagueResults/LeagueResults.js
+++ new-client/src/containers/LeagueResults/LeagueResults.js
@@ -7,11 +7,52 @@ import * as actions from '../../store/actions';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 class LeagueResults extends Component {
-  componentDidMount() {
+  state = {
+    gameweeks: null,
+    weeklyWinners: null
+  };
+
+  componentWillMount() {
     this.props.onFetchPlayers();
   }
 
+  componentWillReceiveProps(newProps) {
+    if (newProps.players !== this.props.players) {
+      this.setState({ gameweeks: this.setGameweekScores(newProps.players) });
+    }
+  }
+
+  setGameweekScores(playersData) {
+    let gameweeks = [];
+    playersData.map(player => {
+      let name = player.entry.name;
+      return player.history.map(gw => {
+        let gameweek = gw.event;
+        let gwScore = {
+          name: name,
+          score: gw.points - gw.event_transfers_cost
+        };
+
+        if (!gameweeks[`Gameweek ${gameweek}`])
+          gameweeks[`Gameweek ${gameweek}`] = [];
+        return gameweeks[`Gameweek ${gameweek}`].push(gwScore);
+      });
+    });
+    return gameweeks;
+  }
+
+  getWinners(gameweeks) {
+    let weeklyWinners = gameweeks.forEach(gameweek => {
+      return gameweek.map(week => week.score);
+    });
+    return weeklyWinners;
+  }
+
   render() {
+    if (this.state.gameweeks) {
+      console.log(this.state.gameweeks);
+    }
+
     return (
       <div className={`container-fluid ${styles.LeagueResults}`}>
         <div className="container">
@@ -22,6 +63,12 @@ class LeagueResults extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    players: state.players.players
+  };
+};
+
 const mapDispatchToProps = dispatch => {
   return {
     onFetchPlayers: () => dispatch(actions.fetchPlayersData())
@@ -29,6 +76,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(LeagueResults);
