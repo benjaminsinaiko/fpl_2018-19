75e88267c0f1c88ea63d242871097588315707da
benjaminsinaiko <ben.jamin@outlook.com>
2018-10-16 01:29:44 -0500

    Add data to overall results table

--- new-client/src/components/WinningsResultsData/WinningsResultsData.js
+++ new-client/src/components/WinningsResultsData/WinningsResultsData.js
@@ -3,24 +3,53 @@ import React from 'react';
 import Table from '../UI/Table/Table';
 
 const winningsResultsData = props => {
-  console.log('props', props.winners);
-  // console.log('props', props.players);
-
-  // Set Players
-  // let teamNames = props.players.map(team => {
-  //   return team.entry.name;
-  // });
-
-  const winnersCount = player => {
-    let winsPerWeek = props.winners.map(week => {
-      return week.lenght;
-
-      if (week.name.includes(player)) {
+  // weekly_wins
+  const weeklyWins = props.players.map(player => {
+    let fullName = `${player.entry.player_first_name} ${
+      player.entry.player_last_name
+    }`;
+    let wins = 0;
+    props.winners.map(week => {
+      let numOfWinners = week.length;
+      if (week[0].name.includes(fullName)) {
+        wins += 1 / numOfWinners;
       }
     });
-  };
+    return wins;
+  });
 
-  console.log(winnersCount);
+  // weekly_winnings
+  const weeklyWinnings = weeklyWins.map(wins => {
+    return wins * (840 / 38);
+  });
+
+  // bonus_penalty
+  const bonusPenalty = [...Array(9).keys()].map(index => {
+    let bonusPenalty = 0;
+    if (index === 0) {
+      bonusPenalty = 900;
+    } else if (index === 1) {
+      bonusPenalty = 562.5;
+    } else if (index === 2) {
+      bonusPenalty = 150;
+    } else if (index === 7) {
+      bonusPenalty = -50;
+    } else if (index === 8) {
+      bonusPenalty = -100;
+    }
+    return bonusPenalty;
+  });
+
+  // Money format
+  const moneyFormat = number => {
+    if (number === 0) {
+      return '-';
+    } else if (number > 0) {
+      return `$${number.toFixed(2)}`;
+    } else {
+      return `($${Math.abs(number.toFixed(2))})`;
+    }
+  };
 
   // Set Columns
   const columns = [
@@ -64,13 +93,13 @@ const winningsResultsData = props => {
   const winningsData = props.players.map((team, index) => {
     return {
       id: index,
-      team: team.entry.name,
-      weekly_wins: '-',
-      weekly_winnings: '-',
-      total_score: '-',
-      bonus_penalty: '-',
-      max_loss: '-',
-      total_pl: '-'
+      team: `${team.entry.player_first_name} ${team.entry.player_last_name}`,
+      weekly_wins: weeklyWins[index],
+      weekly_winnings: moneyFormat(weeklyWinnings[index]),
+      total_score: team.history[team.history.length - 1].total_points,
+      bonus_penalty: moneyFormat(bonusPenalty[index]),
+      max_loss: moneyFormat(-250 + weeklyWinnings[index]),
+      total_pl: moneyFormat(-250 + weeklyWinnings[index] + bonusPenalty[index])
     };
   });
 
