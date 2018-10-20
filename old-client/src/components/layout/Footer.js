940f7a22aa0ec6e1fb72ddba31e337d0d356a80a
benjaminsinaiko <ben.jamin@outlook.com>
2018-10-20 01:30:56 -0500

    Delete old client

--- old-client/src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <footer className="bg mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} @ChicagoSeagulls
-    </footer>
-  );
-};
