4407ef1095b0c6c455aab2e7efa368d18f93a4c3
benjaminsinaiko <ben.jamin@outlook.com>
2018-10-26 14:46:16 -0500

    Change order of value/transfers buttons for better display on mobile

--- client/src/components/PlayerChartsData/PlayerValue/PlayerValue.js
+++ client/src/components/PlayerChartsData/PlayerValue/PlayerValue.js
@@ -11,16 +11,6 @@ const PlayerValue = props => {
     <React.Fragment>
       <div className={`col ${styles.ValueCard}`}>
         <div className="card">
-          <div className={`card-header ${styles.ValueHeader}`}>
-            Total Transfers
-          </div>
-          <div className={`card-body ${styles.ValueBody}`}>
-            <h5 className="card-title">{props.player.total_transfers}</h5>
-          </div>
-        </div>
-      </div>
-      <div className={`col ${styles.ValueCard}`}>
-        <div className="card">
           <div className={`card-header ${styles.ValueHeader}`}>Team Value</div>
           <div className={`card-body ${styles.ValueBody}`}>
             <h5 className="card-title">{formatValue(props.player.value)}</h5>
@@ -32,6 +22,16 @@ const PlayerValue = props => {
           <div className={`card-header ${styles.ValueHeader}`}>In The Bank</div>
           <div className={`card-body ${styles.ValueBody}`}>
             <h5 className="card-title">{formatValue(props.player.bank)}</h5>
+          </div>
+        </div>
+      </div>
+      <div className={`col ${styles.ValueCard}`}>
+        <div className="card">
+          <div className={`card-header ${styles.ValueHeader}`}>
+            Total Transfers
+          </div>
+          <div className={`card-body ${styles.ValueBody}`}>
+            <h5 className="card-title">{props.player.total_transfers}</h5>
           </div>
         </div>
       </div>
