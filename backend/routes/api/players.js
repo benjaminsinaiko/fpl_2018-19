d5aa2f6670bd71edac5869ad52e8bf4e5631818a
benjaminsinaiko <ben.jamin@outlook.com>
2018-11-05 20:19:27 -0600

    Update backend players route to get league sorted by score

--- backend/routes/api/players.js
+++ backend/routes/api/players.js
@@ -2,29 +2,23 @@ const express = require('express');
 const router = new express.Router();
 const axios = require('axios');
 
-// Load Player Model
-const Player = require('../../models/Player');
-
 // @route GET api/players/test
 // @desc Tests players route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Players works' }));
 
-// @route GET api/players/all
-// @desc Get All League Players (API)
+// @route GET api/players
+// @desc Get All League Players (API) - Sorted By Score
 // @access Public
-router.get('/', (req, res) => {
-  const playerIds = [
-    446958,
-    954954,
-    963600,
-    1643394,
-    1790869,
-    3863048,
-    3587639,
-    3357969,
-    3015585
-  ];
+router.get('/', async (req, res) => {
+  const playerIds = await axios
+    .get(
+      'https://fantasy.premierleague.com/drf/leagues-classic-standings/765405'
+    )
+    .then(response => {
+      const leagueIds = response.data.standings.results.map(team => team.entry);
+      return leagueIds;
+    });
 
   const playerPromises = playerIds.map(playerId => {
     return Promise.resolve(
@@ -39,81 +33,6 @@ router.get('/', (req, res) => {
       res.json(response);
     })
     .catch(error => res.json(error));
-});
-
-// @route GET api/players/all
-// @desc Get all League Players (sorted by score)
-// @access Public
-router.get('/all', (req, res) => {
-  const errors = {};
-
-  Player.find()
-    .sort('-history.total_points')
-    .then(players => {
-      if (!players) {
-        errors.noplayers = 'There are no current players';
-        return res.status(404).json(errors);
-      }
-
-      res.json(players);
-    })
-    .catch(err =>
-      res.status(404).json({ players: 'There are no current players' })
-    );
-});
-
-// @route POST api/players/history/:id
-// @desc Update player history
-// @access Public
-router.post('/history/:id', (req, res) => {
-  const newHistory = {
-    entry: req.body.entry,
-    movement: req.body.movement,
-    points: req.body.points,
-    total_points: req.body.total_points,
-    rank: req.body.rank,
-    rank_sort: req.body.rank_sort,
-    overall_rank: req.body.overall_rank,
-    event_transfers: req.body.event_transfers,
-    event_transfers_cost: req.body.event_transfers_cost,
-    value: req.body.value,
-    points_on_bench: req.body.points_on_bench,
-    bank: req.body.bank,
-    event: req.body.event,
-    week_id: req.body.id
-  };
-
-  Player.findOne({ id: req.params.id })
-    .then(player => {
-      if (player.history.length > 0) {
-        let gwIndex = player.history.findIndex(
-          gw => gw.event === newHistory.event
-        );
-        if (gwIndex === -1) {
-          // res.json({ msg: 'not found' });
-          player.history.push(newHistory);
-        } else {
-          player.history[gwIndex] = newHistory;
-        }
-      } else {
-        player.history.push(newHistory);
-      }
-
-      player.save().then(player => res.json(player));
-    })
-    .catch(err => res.status(404).json({ noplayer: 'No player found' }));
-});
-
-// @route GET api/players/gameweeks
-// @desc Get Gameweek scores
-// @access Public
-router.get('/gameweeks', (req, res) => {
-  Player.find()
-    .sort('-history.total_points')
-    .select(
-      'handle history.rank history.overall_rank history.event history.points history.event_transfers_cost'
-    )
-    .then(playersGWS => res.json(playersGWS));
 });
 
 module.exports = router;
